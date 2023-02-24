0f0bb002ff8156f09c7bf82e1ddeef1cca78cab1
dbdrwdn <146220+dbdrwdn@users.noreply.github.com>
2023-02-24 20:07:37 +0100

    initial commit of default bgkeeb with vial (too large with RGB)

---
+++ keyboards/fingerpunch/bgkeeb/keymaps/vial/config.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x2F, 0xD1, 0xB7, 0xCB, 0x14, 0x35, 0xDD, 0x69}
+
+#define VIAL_TAP_DANCE_ENTRIES 4
+#define VIAL_COMBO_ENTRIES 4
+#define VIAL_KEY_OVERRIDE_ENTRIES 4
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
